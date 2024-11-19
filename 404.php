24b0570bea9498ccae49d46638719fe375462e10
Tuan Duc Tran <tuanductran.dev@gmail.com>
2024-11-19 09:40:19 +0700

    Sync update 404.php file
    Add title

--- 404.php
+++ 404.php
@@ -3,6 +3,6 @@
 	<div class="not-found">
 		<h1>404</h1>
 		<p>Page not found</p>
-		<a href="<?php echo esc_url(site_url()); ?>">Go to homepage</a>
+		<a href="<?php echo esc_url(site_url()); ?>" title="Go to homepage">Go to homepage</a>
 	</div>
 </div>
